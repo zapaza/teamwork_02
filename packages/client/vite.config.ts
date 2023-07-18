25ca8dc75b0183acea26c2fa85b1f284b275e0f7
Kosmos <3apa3a.iickov@gmail.com>
2023-07-18 22:04:45 +0300

    fix(typo): fix typo in vite.config.ts

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config()
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
