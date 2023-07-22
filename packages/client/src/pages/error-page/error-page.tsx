1ee359a1183462e215c6987b2f72607f3483aa82
Sammily <sammily03@yandex.ru>
2023-07-22 21:02:40 +0300

    feat: add styles and text to error page (28-16)

--- packages/client/src/pages/error-page/error-page.tsx
+++ packages/client/src/pages/error-page/error-page.tsx
@@ -1,6 +1,6 @@
 import { useRouteError } from 'react-router-dom'
-import './error-page.css';
-import '../../styles/helpers.css';
+import './error-page.css'
+import '../../styles/helpers.css'
 
 export default function ErrorPage() {
   const error = useRouteError()
@@ -8,9 +8,13 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page" className="error-page__container">
-      <h1 className='text-9-xl-font-bold'>{error.status == '404' ? '404' : '500'}</h1>
-      <p className='text-xl-font-regular error-page__description'>
-        {error.status == '404' ? 'То, что вы ищете, не создано или куда-то пропало' : 'Ой, что-то не так :('}
+      <h1 className="text-9-xl-font-bold">
+        {error.status == '404' ? '404' : '500'}
+      </h1>
+      <p className="text-xl-font-regular error-page__description">
+        {error.status == '404'
+          ? 'То, что вы ищете, не создано или куда-то пропало'
+          : 'Ой, что-то не так :('}
       </p>
       <button>На главную</button>
     </div>
