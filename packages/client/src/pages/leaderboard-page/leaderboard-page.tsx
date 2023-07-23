3d43d69e3d23a75f7ee41c8e8c9ee3c49da29369
Sammily <sammily03@yandex.ru>
2023-07-23 15:50:58 +0300

    fix: add helpers classes instead of styles (28-24)

--- packages/client/src/pages/leaderboard-page/leaderboard-page.tsx
+++ packages/client/src/pages/leaderboard-page/leaderboard-page.tsx
@@ -3,7 +3,7 @@ import '../../styles/helpers.css'
 
 function LeaderBoardPage() {
   return (
-    <div className="leaderboard__container">
+    <div className="leaderboard__container flex flex-column flex-ai-center">
       <h5 className="leaderboard__header text-xl-font-bold">Список Лидеров</h5>
       <BaseLeaderboardInput />
     </div>
@@ -12,7 +12,7 @@ function LeaderBoardPage() {
 
 export function BaseLeaderboardInput() {
   return (
-    <div className="leaderboard-input__container">
+    <div className="leaderboard-input__container flex flex-ai-center flex-jc-center">
       <span className="position-number text-base-font-regular">1.</span>
       <span className="user-name text-base-font-regular">Пользователь</span>
       <span className="user-score text-base-font-regular">99999</span>
