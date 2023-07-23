aa5b9f73e4b0fe0037f5bd02075741f9d3ae1f92
Sammily <sammily03@yandex.ru>
2023-07-23 11:51:20 +0300

    feat: add styles to leaderboard page(28-24)

--- packages/client/src/pages/leaderboard-page/leaderboard-page.tsx
+++ packages/client/src/pages/leaderboard-page/leaderboard-page.tsx
@@ -1,5 +1,21 @@
+import './leaderboard-page.css'
+import '../../styles/helpers.css'
+
 function LeaderBoardPage() {
-  return <div>Вот тут будет жить LeaderBoardPage :)</div>
+  return (
+    <div className='leaderboard__container'>
+      <h5 className='leaderboard__header text-xl-font-bold'>Список Лидеров</h5>
+      <BaseLeaderboardInput />
+    </div>)
+}
+
+export function BaseLeaderboardInput() {
+  return (
+    <div className='leaderboard-input__container'>
+      <span className='position-number text-base-font-regular'>1.</span>
+      <span className='user-name text-base-font-regular'>Пользователь</span>
+      <span className='user-score text-base-font-regular'>99999</span>
+    </div>)
 }
 
 export default LeaderBoardPage
