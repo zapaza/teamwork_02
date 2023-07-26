27a70ee7ea3b279473e369188e3adb7c7de11ac5
Alexander <madhamst3r@gmail.com>
2023-07-27 00:53:38 +0300

    feat(AuthReg): finish adding validation

--- packages/client/src/core/validator/index.ts
+++ packages/client/src/core/validator/index.ts
@@ -1,41 +1,68 @@
 import * as yup from 'yup'
 
-export const AuthSchema = yup
-  .object({
-    login: yup.string().required().min(3).max(20)
-      .matches(/^[a-zA-Z0-9-_]*$/, { message: 'Can contain only latin symbol or numbers' })
-      .test(
-        'notNumbers',
-        'Can\'t contain only numbers',
-      (value) => {
-          if (!value) {
-            return false
-          }
-          return !value.match(/^[0-9]*$/)
-        }),
-    password: yup.string().required().min(8).max(40).test(
+export const AuthSchema = yup.object({
+  login: yup
+    .string()
+    .required()
+    .min(3)
+    .max(20)
+    .matches(/^[a-zA-Z0-9-_]*$/, {
+      message: 'Can contain only latin symbol or numbers',
+    })
+    .test('notNumbers', "Can't contain only numbers", value => {
+      if (!value) {
+        return false
+      }
+      return !value.match(/^[0-9]*$/)
+    }),
+  password: yup
+    .string()
+    .required()
+    .min(8)
+    .max(40)
+    .test(
       'containNumberAndUpperCaseSymbol',
       'Must contain number and upper case symbol',
-      (value) => !!value.match(/[0-9]/)  && !!value.match(/[A-Z]/)
+      value => !!value.match(/[0-9]/) && !!value.match(/[A-Z]/)
     ),
-    repeat_password: yup.string().required().oneOf([yup.ref('password')], 'Passwords must match'),
-    first_name: yup.string().required()
-      .matches(/^[a-zA-Zа-яА-Я-]*$/, { message: 'Can\'t contain space and special symbols' })
-      .test(
+  repeat_password: yup
+    .string()
+    .required()
+    .oneOf([yup.ref('password')], 'Passwords must match'),
+  first_name: yup
+    .string()
+    .required()
+    .matches(/^[a-zA-Zа-яА-Я-]*$/, {
+      message: "Can't contain space and special symbols",
+    })
+    .test(
       'firstSymbolUpperCase',
       'First symbol must be to upper case',
-      (value) => !!value[0].match(/^[A-ZА-Я]*$/)),
-    second_name: yup.string().required()
-      .matches(/^[a-zA-Zа-яА-Я-]*$/, { message: 'Can\'t contain space and special symbols' })
-      .test(
-        'firstSymbolUpperCase',
-        'First symbol must be to upper case',
-        (value) => !!value[0].match(/^[A-ZА-Я]*$/)),
-    email: yup.string().required().email()
-      .matches(/.+@.+\..+/, 'Invalid email'),
-    phone: yup.string().required()
-      .matches(/\d+|\+\d+|\+\d\(\d{3}\)\d{7}/, "Can be only numbers, may start from +")
-      .min(10).max(15),
-  })
-
-
+      value => !!value[0].match(/^[A-ZА-Я]*$/)
+    ),
+  second_name: yup
+    .string()
+    .required()
+    .matches(/^[a-zA-Zа-яА-Я-]*$/, {
+      message: "Can't contain space and special symbols",
+    })
+    .test(
+      'firstSymbolUpperCase',
+      'First symbol must be to upper case',
+      value => !!value[0].match(/^[A-ZА-Я]*$/)
+    ),
+  email: yup
+    .string()
+    .required()
+    .email()
+    .matches(/.+@.+\..+/, 'Invalid email'),
+  phone: yup
+    .string()
+    .required()
+    .matches(
+      /\d+|\+\d+|\+\d\(\d{3}\)\d{7}/,
+      'Can be only numbers, may start from +'
+    )
+    .min(10)
+    .max(15),
+})
