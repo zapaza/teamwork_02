d94df37d89eebf02c36f9292a82845ff987af44d
Sammily <sammily03@yandex.ru>
2023-07-27 16:21:11 +0300

    fix: fix code style (28-21)

--- packages/client/src/components/ui/topic/topic.tsx
+++ packages/client/src/components/ui/topic/topic.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import './topic.pcss'
 
 export type TopicType = {
-  id: string;
-  topicTitle: string;
-  topicText: string;
+  id: string
+  topicTitle: string
+  topicText: string
 }
 
 function Topic(props: TopicType) {
   return (
     <div className="topic__container flex flex-column">
-      <h3 className='topic__header'>{props.topicTitle}</h3>
+      <h3 className="topic__header">{props.topicTitle}</h3>
       <p>{props.topicText}</p>
     </div>
   )
--- packages/client/src/pages/forum/forum-page.tsx
+++ packages/client/src/pages/forum/forum-page.tsx
@@ -2,32 +2,48 @@ import Button from '../../components/ui/button/button'
 import Topic from '../../components/ui/topic/topic'
 import './forum-page.pcss'
 
-const forumMock = [{
-  id: "1",
-  topicTitle: "Заголовок топика1",
-  topicText: "Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика"
-}, {
-  id: "2",
-  topicTitle: "Заголовок топика2",
-  topicText: "Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика"
-}, {
-  id: "3",
-  topicTitle: "Заголовок топика3",
-  topicText: "Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика"
-}, {
-  id: "4",
-  topicTitle: "Заголовок топика4",
-  topicText: "Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика"
-}]
+const forumMock = [
+  {
+    id: '1',
+    topicTitle: 'Заголовок топика1',
+    topicText:
+      'Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика',
+  },
+  {
+    id: '2',
+    topicTitle: 'Заголовок топика2',
+    topicText:
+      'Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика',
+  },
+  {
+    id: '3',
+    topicTitle: 'Заголовок топика3',
+    topicText:
+      'Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика',
+  },
+  {
+    id: '4',
+    topicTitle: 'Заголовок топика4',
+    topicText:
+      'Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика',
+  },
+]
 
 function ForumPage() {
-  return <div className='forum__container flex'>
-    <div className="all-topic-container flex flex-column">
-      {forumMock.map(item => (
-        <Topic key={item.id} {...item} />
-      ))}
+  return (
+    <div className="forum__container flex">
+      <div className="all-topic-container flex flex-column">
+        {forumMock.map(item => (
+          <Topic key={item.id} {...item} />
+        ))}
+      </div>
+      <Button
+        name="createTopicBtn"
+        children="Создать топик"
+        className="button text-base-font-regular create-topic-btn"
+      />
     </div>
-    <Button name="createTopicBtn" children="Создать топик" className='button text-base-font-regular create-topic-btn' /></div>
+  )
 }
 
 export default ForumPage
