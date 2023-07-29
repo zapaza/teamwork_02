642694c178c1426b4e8ed81de335cc0d91cafd4e
Sammily <sammily03@yandex.ru>
2023-07-29 12:06:14 +0300

    feat: add forum-topic component and styles (28-21)

---
+++ packages/client/src/components/ui/comment/comment.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './comment.pcss'
+
+export type CommentPropsType = {
+  id: string
+  userName: string
+  commentText: string
+  date: Date
+}
+
+function Comment(props: CommentPropsType) {
+  return (
+    <div className="comment__container flex flex-column">
+      <div className="comment__header flex flex-jc-sb">
+        <h3 className="comment__username text-xl-font-bold">
+          {props.userName}
+        </h3>
+        <span className="comment__date">{props.date.toLocaleDateString()}</span>
+      </div>
+      <p className="comment__text text-base-font-regular">
+        {props.commentText}
+      </p>
+    </div>
+  )
+}
+
+export default Comment
--- packages/client/src/pages/forum-topic/forum-topic-page.tsx
+++ packages/client/src/pages/forum-topic/forum-topic-page.tsx
@@ -1,5 +1,47 @@
+import Topic from '../../components/ui/topic/topic'
+import Comment from '../../components/ui/comment/comment'
+import './forum-topic-page.pcss'
+import Button from '../../components/ui/button/button'
+
+const topicMock = {
+  id: '1',
+  topicTitle: 'Заголовок топика1',
+  topicText:
+    'Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика Полный текст топика',
+}
+
+const commentsMock = [
+  { id: '1', userName: 'Иван', commentText: 'Игра топ', date: new Date() },
+  {
+    id: '2',
+    userName: 'Коля',
+    commentText: 'Команда красавцы',
+    date: new Date(),
+  },
+]
+
+const newCommentMock =
+  'Новый комментарий Новый комментарий Новый комментарий Новый комментарий Новый комментарий Новый комментарий Новый комментарий Новый комментарий Новый комментарий Новый комментарий Новый комментарий Новый комментарий Новый комментарий Новый комментарий'
+
 function ForumTopicPage() {
-  return <div>Вот тут будет жить ForumTopicPage :)</div>
+  return (
+    <div className="forum-topic__container flex flex-column flex-ai-center">
+      <Topic key={topicMock.id} {...topicMock} />
+      <h5 className="comments-header text-base-font-bold">Комментарии:</h5>
+      {commentsMock.map(item => (
+        <Comment {...item} />
+      ))}
+      <form className="new-comment-form form__container flex flex-column">
+        <h5 className="text-xl-font-bold">Оставить комментарий</h5>
+        <textarea className="comments__textarea">{newCommentMock}</textarea>
+        <Button
+          name="addCommentBtn"
+          children="Отправить"
+          className="button add-comment-button"
+        />
+      </form>
+    </div>
+  )
 }
 
 export default ForumTopicPage
