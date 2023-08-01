98ca4783916016738e87fc0da7f3bf48daafddcc
Sammily <sammily03@yandex.ru>
2023-08-01 13:15:05 +0300

    fix: does not redirect to main when refresh page

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,15 +1,30 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/header/header'
 import { Outlet } from 'react-router-dom'
 import { checkAuth } from './store/auth/authSlice'
 import { useAppDispatch } from './store'
 
 function App() {
+  const [isDataLoaded, setDataLoaded] = useState(false);
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    dispatch(checkAuth())
+    const fetchAuth = async () => {
+      try {
+        await dispatch(checkAuth()).unwrap()
+        setDataLoaded(true);
+      } catch (error) {
+        console.error(error)
+        setDataLoaded(true);
+      }
+    }
+    fetchAuth()
   }, [dispatch])
+
+  if (!isDataLoaded) {
+    return null;
+  }
+
   return (
     <>
       <Header />
