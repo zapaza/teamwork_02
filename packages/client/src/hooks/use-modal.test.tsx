81819f62ae39ee5c769f268ab2c87b72b357bd74
Sammily <sammily03@yandex.ru>
2023-08-08 18:14:50 +0300

    fix: codestyle

--- packages/client/src/hooks/use-modal.test.tsx
+++ packages/client/src/hooks/use-modal.test.tsx
@@ -4,28 +4,28 @@ import { fireEvent, render, screen } from '@testing-library/react'
 
 describe('useModal hook', () => {
   function TestComponent() {
-    const { isOpen, toggle } = useModal();
+    const { isOpen, toggle } = useModal()
     return <button onClick={toggle}>{isOpen.toString()}</button>
   }
 
   test('initial value isOpen is false', () => {
-    render(<TestComponent />);
-    expect(screen.getByText('false')).toBeInTheDocument();
-  });
-  
+    render(<TestComponent />)
+    expect(screen.getByText('false')).toBeInTheDocument()
+  })
+
   test('button click should update isOpen to true', () => {
-    render(<TestComponent />);
-    fireEvent.click(screen.getByText('false'));
+    render(<TestComponent />)
+    fireEvent.click(screen.getByText('false'))
     expect(screen.getByText('true')).toBeInTheDocument()
-  });
+  })
 
   test('check if isOpen can toggle', () => {
-    render(<TestComponent />);
-    fireEvent.click(screen.getByText('false'));
+    render(<TestComponent />)
+    fireEvent.click(screen.getByText('false'))
     expect(screen.getByText('true')).toBeInTheDocument()
-    fireEvent.click(screen.getByText('true'));
-    expect(screen.getByText('false')).toBeInTheDocument();
-    fireEvent.click(screen.getByText('false'));
+    fireEvent.click(screen.getByText('true'))
+    expect(screen.getByText('false')).toBeInTheDocument()
+    fireEvent.click(screen.getByText('false'))
     expect(screen.getByText('true')).toBeInTheDocument()
-  });
-});
+  })
+})
