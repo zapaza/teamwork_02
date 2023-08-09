e0c03b503dfe63a838e0537ce4f1253ceb6725fa
Kustov Ilia <kustoff.il@yandex.ru>
2023-08-09 16:46:21 +0300

    feat: add page for game start (28-25)

--- packages/client/src/pages/game/game-page.tsx
+++ packages/client/src/pages/game/game-page.tsx
@@ -1,11 +1,36 @@
-import GameCanvas from '../../components/game/gameState/gameState'
+import { useState } from 'react'
+import Button from '../../components/ui/button/button'
 import './game-page.pcss'
+import GameCanvas from '../../components/game/gameState/gameState'
+
+const GAME_LOADING_TIMER = 3000
 
 function GamePage() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const onClick = () => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+      setIsPlaying(true)
+    }, GAME_LOADING_TIMER)
+  }
+
   return (
-    <div className="game-page">
-      <GameCanvas />
-    </div>
+    <main className="game-page flex flex-jc-center">
+      <div className="game-page__container">
+        {!isPlaying && (
+          <div className="game-page__overlay flex flex-jc-center flex-ai-center">
+            {isLoading && <div className="game-page__loading-spinner"></div>}
+            {!isLoading && (
+              <Button name="play" children="Играть" onClick={onClick} />
+            )}
+          </div>
+        )}
+        <GameCanvas />
+      </div>
+    </main>
   )
 }
 
