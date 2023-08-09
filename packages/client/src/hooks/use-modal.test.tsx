6f8cf937ff218fc64e44fb7367d5175aacc713c9
Sammily <sammily03@yandex.ru>
2023-08-09 12:27:10 +0300

    fix: move component render to beforeEach (28-41)

--- packages/client/src/hooks/use-modal.test.tsx
+++ packages/client/src/hooks/use-modal.test.tsx
@@ -2,25 +2,26 @@ import '@testing-library/jest-dom/extend-expect'
 import useModal from './use-modal'
 import { fireEvent, render, screen } from '@testing-library/react'
 
+function TestComponent() {
+  const { isOpen, toggle } = useModal()
+  return <button onClick={toggle}>{isOpen.toString()}</button>
+}
+
 describe('useModal hook', () => {
-  function TestComponent() {
-    const { isOpen, toggle } = useModal()
-    return <button onClick={toggle}>{isOpen.toString()}</button>
-  }
+  beforeEach(() => {
+    render(<TestComponent />)
+  })
 
   test('initial value isOpen is false', () => {
-    render(<TestComponent />)
     expect(screen.getByText('false')).toBeInTheDocument()
   })
 
   test('button click should update isOpen to true', () => {
-    render(<TestComponent />)
     fireEvent.click(screen.getByText('false'))
     expect(screen.getByText('true')).toBeInTheDocument()
   })
 
   test('check if isOpen can toggle', () => {
-    render(<TestComponent />)
     fireEvent.click(screen.getByText('false'))
     expect(screen.getByText('true')).toBeInTheDocument()
     fireEvent.click(screen.getByText('true'))
