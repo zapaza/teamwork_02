30927ada3eb7cc9bdefcccd9a8e87705fdcb2a61
Alexander <madhamst3r@gmail.com>
2023-08-14 00:08:08 +0300

    fix(core): fix styling

--- packages/client/src/pages/game/game-page.tsx
+++ packages/client/src/pages/game/game-page.tsx
@@ -28,7 +28,7 @@ function GamePage() {
   }, [state])
 
   return (
-    <main className="game-page flex flex-jc-center">
+    <main className="game-page flex flex-jc-center flex-ai-center">
       <div className="game-page__container">
         {!isPlaying && (
           <div className="game-page__overlay flex flex-jc-center flex-ai-center">
