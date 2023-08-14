3e575b8c6f695190e7ac502b7cc82bd1a9b4c479
Peter Tolkachev <peter.tolkachev@gmail.com>
2023-08-14 15:15:46 +0300

    fix code style

--- packages/client/src/service-worker.ts
+++ packages/client/src/service-worker.ts
@@ -1,7 +1,6 @@
 // prettier-ignore
 
 const CACHE_NAME = 'version-1'
-// список урлов, которые хотим закэшировать
 const routesToCache = [
   '/',
   '/main',
@@ -15,44 +14,53 @@ const routesToCache = [
 ]
 
 self.addEventListener('install', event => {
-  (event as InstallEvent).waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(routesToCache)
-    })
-  )
+  (event as InstallEvent).waitUntil(cacheRoutes())
 })
 
 self.addEventListener('fetch', event => {
-  (event as FetchEvent).respondWith(
-    caches.match((event as FetchEvent).request).then(cachedResponse => {
-      // Если ресурс в кэше, возвращаем его.
-      if (cachedResponse) {
-        return cachedResponse
-      }
-
-      // Иначе, делаем запрос к серверу.
-      return fetch((event as FetchEvent).request).then(response => {
-        // Кэшируем ответ для будущего.
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put((event as FetchEvent).request, response.clone())
-          return response
-        })
-      })
-    })
-  )
+  (event as FetchEvent).respondWith(fetchOrFallback(event as FetchEvent))
 })
 
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  (event as ExtendableEvent).waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName)
-          }
-        })
-      )
+  (event as ExtendableEvent).waitUntil(cleanOldCaches())
+})
+
+async function cacheRoutes() {
+  try {
+    const cache = await caches.open(CACHE_NAME)
+    return cache.addAll(routesToCache)
+  } catch (error) {
+    console.error("Error caching routes:", error)
+  }
+}
+
+async function fetchOrFallback(event: FetchEvent): Promise<Response> {
+  try {
+    const cachedResponse = await caches.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+
+    const response = await fetch(event.request);
+    const cache = await caches.open(CACHE_NAME);
+    await cache.put(event.request, response.clone());
+    return response;
+
+  } catch (error) {
+    console.error("Error fetching or falling back:", error);
+    return new Response('Service Worker Fetch Error', { status: 500 });
+  }
+}
+
+
+async function cleanOldCaches() {
+  const cacheNames = await caches.keys()
+  const cacheWhitelist = [CACHE_NAME]
+  return Promise.all(
+    cacheNames.map(async cacheName => {
+      if (!cacheWhitelist.includes(cacheName)) {
+        return caches.delete(cacheName)
+      }
     })
   )
-})
+}
