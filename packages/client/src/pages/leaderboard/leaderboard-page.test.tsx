dccdb1643f3a3afe4dff1da3463878324eaa1ec6
Sammily <sammily03@yandex.ru>
2023-08-27 19:03:39 +0300

    fix: fix file name

--- packages/client/src/pages/leaderboard/leaderboard-page.test.tsx
+++ packages/client/src/pages/leaderboard/leaderboard-page.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { LeaderboardPage } from '@/pages/leaderboard/leaderboardPage';
+import { LeaderboardPage } from '@/pages/leaderboard/leaderboard-page';
 
 describe('Leaderboard component', () => {
 	test('component render', () => {
-		render(<LeaderboardPage/>);
+		render(<LeaderboardPage />);
 
 		expect(screen.getByRole('heading').textContent).toBe('Список Лидеров');
 		expect(screen.getByText('10.')).toBeInTheDocument();
