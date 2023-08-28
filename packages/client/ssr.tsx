cea6a8bc5ae83ea8483579ec12f167442fa3009e
stanislavcumakov <3apa3a.iickov@gmail.com>
2023-08-28 20:08:08 +0300

    feat(ssr): add ssr-build to project

---
+++ packages/client/ssr.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import { Provider } from 'react-redux';
+import store from './src/store';
+import { RouterProvider } from 'react-router-dom';
+import router from './src/routes';
+
+export const render = (url: string) => {
+
+	return renderToString(
+		<React.Suspense fallback='Loading...'>
+			<StaticRouter location={url}>
+				<Provider store={store}>
+					<RouterProvider router={router}/>
+				</Provider>
+			</StaticRouter>
+		</React.Suspense>,
+	);
+};
