f960b28726b702d9c07108c9dadca23acb3bf35f
Sammily <sammily03@yandex.ru>
2023-09-08 21:53:17 +0300

    feat: add slice, start api-forum

---
+++ packages/client/src/store/forum/forumSlice.ts
@@ -0,0 +1,23 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { fetchAllTopics } from './forumThunk';
+
+export const forumSlice = createSlice({
+	name: 'forum',
+	initialState: {},
+	reducers: {},
+	extraReducers: builder => {
+		builder
+			.addCase(
+				fetchAllTopics.fulfilled,
+				(state, action) => {
+					//state.data = action.payload;
+					//state.isDataLoaded = true;
+				},
+			)
+			.addCase(fetchAllTopics.rejected, () => {
+				console.error('fetch topics failed');
+			});
+	},
+});
+
+export default forumSlice.reducer;
---
+++ packages/client/src/store/forum/forumThunk.ts
@@ -0,0 +1,14 @@
+import { TopicType } from '@/components/ui/topic/topic';
+import { apiForum } from '@/core/api/api-forum';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchAllTopics = createAsyncThunk('forum/allTopics', async (_, thunkAPI) => {
+	try {
+		const response = (await apiForum.getAllTopics()) as TopicType[];
+		return response;
+	} catch (error) {
+		return thunkAPI.rejectWithValue({
+			error: (error as Error | null)?.message,
+		});
+	}
+});
