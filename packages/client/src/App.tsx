c620176b3ef22117d0778ac5ffee94b6a7c579dc
stanislavcumakov <3apa3a.iickov@gmail.com>
2023-10-07 14:00:52 +0300

    feat(sw): fix sw

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,7 +3,11 @@ import { Outlet } from 'react-router-dom';
 import { RootState } from './store';
 import { useSelector } from 'react-redux';
 import { useAuth } from './hooks/use-auth';
+import { registerServiceWorker } from '@/utils/serviceWorker';
 
+if (process.env.NODE_ENV === 'production') {
+	registerServiceWorker();
+}
 export const App = () => {
 	const isDataLoaded = useSelector((state: RootState) => state.auth.isDataLoaded);
 
